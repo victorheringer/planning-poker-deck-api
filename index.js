9ffb25f22c8f22e7ade7a67cddc9441812479ccf
Victor Heringer <victorhugo.heringer@hotmail.com>
2022-12-26 19:30:39 -0300

    Refactor for better readability

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const io = require("socket.io")(process.env.PORT, {
   methods: ["GET", "POST"],
 });
 
-const hostRoom = (room) => `host/${room}`;
-const clientRoom = (room) => `client/${room};`;
-
-const Actions = {
+const ACTIONS = {
   JOIN_ROOM: "JOIN_ROOM",
   REMOVE_DISCONNECTED_CLIENT: "REMOVE_DISCONNECTED_CLIENT",
   DISCONNECT_CLIENTS_FROM_HOST: "DISCONNECT_CLIENTS_FROM_HOST",
@@ -20,55 +17,73 @@ const Actions = {
   HOST_CONNECTED: "HOST_CONNECTED",
 };
 
-io.on("connection", (socket) => {
-  socket.on(Actions.JOIN_ROOM, function (payload) {
-    const roomHost = hostRoom(payload.room);
-    const roomClient = clientRoom(payload.room);
+const IO_EVENTS = {
+  DISCONNECT: "disconnecting",
+  CONNECT: "connection",
+};
 
-    if (payload.owner) {
-      socket.join(roomHost);
+function getHostRoom(room) {
+  return `host/${room}`;
+}
 
-      io.to(roomClient).emit(Actions.CLIENT, {
-        type: Actions.HOST_CONNECTED,
+function getClientRoom(room) {
+  return `client/${room};`;
+}
+
+function getAllRooms(room) {
+  return { hostRoom: getHostRoom(room), clientRoom: getClientRoom(room) };
+}
+
+function getInitClientState(id, playerName, socketId) {
+  return {
+    clientState: { id, playerName, card: null, online: true, socketId },
+  };
+}
+
+io.on(IO_EVENTS.CONNECT, (socket) => {
+  socket.on(ACTIONS.JOIN_ROOM, ({ owner, room, id, playerName }) => {
+    const { hostRoom, clientRoom } = getAllRooms(room);
+
+    function joinHost() {
+      socket.join(hostRoom);
+
+      io.to(clientRoom).emit(ACTIONS.CLIENT, {
+        type: ACTIONS.HOST_CONNECTED,
       });
 
-      socket.on("disconnecting", (payload) => {
-        io.to(roomClient).emit(Actions.CLIENT, {
-          type: Actions.DISCONNECT_CLIENTS_FROM_HOST,
+      socket.on(IO_EVENTS.DISCONNECT, () => {
+        io.to(clientRoom).emit(ACTIONS.CLIENT, {
+          type: ACTIONS.DISCONNECT_CLIENTS_FROM_HOST,
         });
       });
-    } else {
-      socket.join(roomClient);
+    }
 
-      socket.broadcast.to(roomHost).emit(Actions.HOST, {
-        type: Actions.CLIENT_JOIN_GAME,
-        payload: {
-          clientState: {
-            id: payload.id,
-            socketId: socket.id,
-            card: null,
-            online: true,
-            playerName: payload.playerName,
-          },
-        },
+    function joinClient() {
+      socket.join(clientRoom);
+
+      socket.broadcast.to(hostRoom).emit(ACTIONS.HOST, {
+        type: ACTIONS.CLIENT_JOIN_GAME,
+        payload: getInitClientState(id, playerName, socket.id),
       });
 
-      socket.on("disconnecting", (payload) => {
-        io.to(roomHost).emit(Actions.HOST, {
-          type: Actions.REMOVE_DISCONNECTED_CLIENT,
+      socket.on(IO_EVENTS.DISCONNECT, () => {
+        io.to(hostRoom).emit(ACTIONS.HOST, {
+          type: ACTIONS.REMOVE_DISCONNECTED_CLIENT,
           payload: { socketId: socket.id },
         });
       });
     }
+
+    owner ? joinHost() : joinClient();
   });
 
-  socket.on(Actions.CALL_HOST, function (action) {
-    io.to(hostRoom(action.payload.room)).emit(Actions.HOST, action);
+  socket.on(ACTIONS.CALL_HOST, (action) => {
+    io.to(getHostRoom(action.payload.room)).emit(ACTIONS.HOST, action);
   });
 
-  socket.on(Actions.CALL_CLIENT, function (action) {
-    io.to(clientRoom(action.payload.room)).emit(Actions.CLIENT, action);
+  socket.on(ACTIONS.CALL_CLIENT, (action) => {
+    io.to(getClientRoom(action.payload.room)).emit(ACTIONS.CLIENT, action);
   });
 });
 
-console.log("Running server");
+console.log("Running planning poker server");
